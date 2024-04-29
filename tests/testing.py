116a037ac1b80bafef98a0dc0fda35bae08b326f
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-04-29 23:23:07 +0200

    Created new tests to test whole pipeline

--- tests/testing.py
+++ tests/testing.py
@@ -4,7 +4,7 @@ import sys
 # Add the root directory to sys.path to find the main module
 sys.path.append('..')
 
-from main import generating_algebraic_template
+from main import *
 from test_cases import *
 
 class Test(unittest.TestCase):
@@ -13,6 +13,14 @@ class Test(unittest.TestCase):
             question_template, mapping = generating_algebraic_template(test['q'])
             self.assertEqual(question_template, test['qt'], "The template does not match the expected template.")
             self.assertEqual(mapping, test['Mapping'], "The mapping does not match the expected mapping.")
+    
+    def test_compute_verification(self):
+        for test in test_cases:
+            qt, variable_mapping = generating_algebraic_template(test['q'])
+            exp = algebric_expression_generation(qt)
+            code= python_code_generation(qt)
+            res = perform_computational_verification(exp, code, variable_mapping)
+            self.assertEqual(res, test['Correct'], "Not the correct answer.")
 
 if __name__ == '__main__':
     unittest.main()
