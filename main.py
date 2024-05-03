ffb01be332d6e6bcc91cd7f20ea18cbcda992eea
Ramon Kaspar <kasparr@stundent.ethz.ch>
2024-05-03 11:38:10 +0200

    Renamed variables

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from generating_algebraic_template import generating_algebraic_template
 from math_prompts_generation import algebric_expression_generation, python_code_generation
 from compute_verification import perform_computational_verification
 
-def mathprompter(question: str, iterations=5, sample_size=5):
+def mathprompter(question: str, self_consistency_iterations=5, n_test_random_values=5):
     """
     Attempts to verify a mathematical prompt via repeated computations and returns the most frequently computed result.
     Parameters:
         question (str): The mathematical question in natural-language form.
-        iterations (int): Number of times the verification should be performed.
-        sample_size (int): Number of random samples to test for each verification.
+        self_consistency_iterations (int): Number of times the verification should be performed.
+        n_test_random_values (int): Number of random samples to test for each verification.
 
     Returns:
         The most frequently computed result over iterations, or None if no consistent result was found.
     """
     result_counts = {}
-    for _ in range(iterations):     # TODO: Parallelize this
+    for _ in range(self_consistency_iterations):     # TODO: Parallelize this
         try:
             qt, variable_mapping = generating_algebraic_template(question)
             exp = algebric_expression_generation(qt)
-            code= python_code_generation(qt)
-            res = perform_computational_verification(exp, code, variable_mapping, sample_size=sample_size)
+            code = python_code_generation(qt)
+            res = perform_computational_verification(exp, code, variable_mapping, sample_size=n_test_random_values)
             if res is not None:
                 if res in result_counts:
                     result_counts[res] += 1
@@ -39,6 +39,6 @@ def mathprompter(question: str, iterations=5, sample_size=5):
 # Example usage
 if __name__ == '__main__':
     question = 'At a restaurant, each adult meal costs $5 and kids eat free. If a group of 15 people came in and 8 were kids, how much would it cost for the group to eat?'
-    res = mathprompter(question)
+    res = mathprompter(question,self_consistency_iterations=1, n_test_random_values=3)
     print(f"Result: {res}")
     
